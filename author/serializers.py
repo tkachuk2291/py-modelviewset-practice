163671d07c739a91cc21d19373a82c1954bd267b
Anton Tkachuk <tkacuk2291@gmail.com>
2024-04-15 13:51:16 +0300

    solution

--- author/serializers.py
+++ author/serializers.py
@@ -4,7 +4,6 @@ from author.models import Author
 
 
 class AuthorSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
     first_name = serializers.CharField(max_length=64)
     last_name = serializers.CharField(max_length=64)
     pseudonym = serializers.CharField(
